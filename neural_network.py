da5f549f0f3b4a2b0b86f7695b3ae6e2bd49cf30
Chocalatey-Mai <S.Alnouri0612@gmail.com>
2026-09-18 11:05:24 +0200

    Added param-init func

--- neural_network.py
+++ neural_network.py
@@ -9,6 +9,20 @@ def init_data():
     return data_df, data
 
 
+def init_parameters(data):
+
+    rng = np.random.default_rng()
+
+    X = data
+
+    w1 = rng.normal(loc=0, scale=np.sqrt(2))
+    b1 = 0
+    w2 = rng.normal(loc=0, scale=np.sqrt(2))
+    b2 = 0
+
+    return X, w1, b1, w2, b2
+
+
 def main():
     data_df, data = init_data()  # inner row in big list: sample
 
