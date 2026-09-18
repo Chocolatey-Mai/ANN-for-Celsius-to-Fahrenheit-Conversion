460dfbd7282bc81ee6da6cd663821029f65cf610
Chocalatey-Mai <S.Alnouri0612@gmail.com>
2026-09-18 10:46:35 +0200

    Created file and init data

--- neural_network.py
+++ neural_network.py
@@ -1,1 +1,17 @@
-...
+import numpy as np
+import pandas as pd
+
+
+def init_data():
+    data_df = pd.read_csv("celsius_to_fahrenheit.csv")
+    data = data_df.to_numpy()
+
+    return data_df, data
+
+
+def main():
+    data_df, data = init_data()  # inner row in big list: sample
+
+
+if __name__ == "__main__":
+    main()
